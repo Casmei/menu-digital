e39dde730f9d044fceb7d41dd1fb523ad9e7d512
ikaro-hans <ikaro-hans@hotmail.com.br>
2024-08-01 15:42:47 -0300

    feat(produto): creat struture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,35 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <div className="p-6 border border-gray-200 rounded-lg">
-        <img
-          src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGl6emF8ZW58MHx8MHx8fDA%3D"
-          alt="Pizza"
-        />
-        <div>
-          <h2>Pizza Calabresa</h2>
-        </div>
-        <div>
-          <button>
-            <i></i>
-            <p>$19.20</p>
-          </button>
-        </div>
+      <div className="container">
+        <a href="">
+          <div className="">
+            <div className="content">
+              <div className="name">
+                <h2>Pizza</h2>
+              </div>
+              <div className="description">
+                <p>
+                  Pão, Blend 130g, 2 fatias de queijo cheddar, Costela, 2 aneis
+                  de cebola empanada, cebola crispy, maionese, molho bauer.
+                </p>
+              </div>
+              <div className="price">
+                <p>R$25</p>
+              </div>
+            </div>
+            <div className="image">
+              <Image
+                src={"/image/pizza.jpg"}
+                alt="Bauer Costela"
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+        </a>
       </div>
     </>
   );
